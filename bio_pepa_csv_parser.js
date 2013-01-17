37a18ea3291267993349dc69ee7ed7dcd2c5f786
James Hulme <s0901522@sms.ed.ac.uk>
2013-01-17 15:38:46 +0000

    now checks whether a valid file type

--- bio_pepa_csv_parser.js
+++ bio_pepa_csv_parser.js
@@ -1,23 +1,21 @@
 function prep(){
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-      alert('File APIs are supported');
-    } else {
-      alert('The File APIs are not fully supported in this browser.');
+    if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+        alert('The File APIs are not fully supported in this browser.');
     }
-    alert('fuck');
-    //document.getElementById('csvbp').addEventListener('change', readFile, false);
-    alert('fuck');
+    
+    document.getElementById('csvbp').addEventListener('change', readFile, false);
 }
 
 function readFile(evt) { 
     var files = evt.target.files;
     var output = [];
     for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-                  '</li>');
+        if (escape(f.type) != 'text/csv') {
+            document.getElementById('file_warnings').innerHTML = 'Some files ignored due to incorrect type';
+        } else {
+            output.push('<li><strong>', escape(f.name), '</strong> </li>');
+        }
     }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+    document.getElementById('file_list').innerHTML = '<ul>' + output.join('') + '</ul>';
 }
 
