9fc9dc7a822b996c9a728afe06d8d1847d44b5e1
James Hulme <s0901522@sms.ed.ac.uk>
2013-01-17 11:57:48 +0000

    can select files

--- bio_pepa_csv_parser.js
+++ bio_pepa_csv_parser.js
@@ -1,3 +1,23 @@
+function prep(){
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      alert('File APIs are supported');
+    } else {
+      alert('The File APIs are not fully supported in this browser.');
+    }
+    alert('fuck');
+    document.getElementById('csvbp').addEventListener('change', readFile, false);
+    alert('fuck');
+}
 
+function readFile(evt) { 
+    var files = evt.target.files;
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                  '</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+}
 
-window.onload = codeAddress;
