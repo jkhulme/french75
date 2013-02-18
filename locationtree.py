3b3e62183dccdd1872ec8c79ff1cb53d8ada385c
James Hulme <s0901522@sms.ed.ac.uk>
2013-02-18 21:17:04 +0000

    renamed stack to queue in locationtree

--- locationtree.py
+++ locationtree.py
@@ -5,7 +5,7 @@ class LocationTree:
     def __init__(self, location_data):
         self.loc_data = location_data
         self.tree = {}
-        self.stack = []
+        self.queue = []
 
     def build_tree(self):
         for loc in self.loc_data:
@@ -16,11 +16,11 @@ class LocationTree:
         print self.tree
 
     def draw_tree(self):
-        self.stack += [self.tree['root'][0][:-1]]
-        while (len(self.stack) > 0):
-            node = self.stack.pop(0)
+        self.queue += [self.tree['root'][0][:-1]]
+        while (len(self.queue) > 0):
+            node = self.queue.pop(0)
             print node
             try:
-                self.stack += self.tree[node]
+                self.queue += self.tree[node]
             except:
                 print "leaf"
