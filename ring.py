ba2bebdafa57318004ab0d16298455b8806d844f
James Hulme <s0901522@sms.ed.ac.uk>
2013-02-25 13:49:30 +0000

    getting rotation working - rotating around the new coordinates each time, need to rotate around original coordinates

--- ring.py
+++ ring.py
@@ -1,3 +1,4 @@
+import math
 
 
 class Ring:
@@ -10,12 +11,14 @@ class Ring:
         self.cell_x = inner[0]
         self.cell_y = inner[1]
         self.cell_radius = inner[2]
+        self.child_x = self.cell_x + self.cell_radius - ((self.radius - self.cell_radius) / 4)
+        self.child_y = self.cell_y + self.cell_radius - ((self.radius - self.cell_radius) / 4)
+        self.child_radius = ((self.radius - self.cell_radius) / 2) - 3
 
     def paint(self):
         self.dc.DrawCircle(self.x, self.y, self.radius)
 
     def give_birth(self):
-        radius = ((self.radius - self.cell_radius) / 2) - 3
-        x = self.cell_x + self.cell_radius - ((self.radius - self.cell_radius) / 4)
-        y = self.cell_y + self.cell_radius - ((self.radius - self.cell_radius) / 4)
-        return (x, y, radius)
+        new_x = (self.child_x * math.cos(math.radians(20))) - (self.child_y * math.sin(math.radians(20)))
+        new_y = (self.child_x * math.sin(math.radians(20))) + (self.child_y * math.cos(math.radians(20)))
+        return (new_x, new_y, self.child_radius)
