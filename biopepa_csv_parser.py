a776522fea65446a2229ed157fbef3c21a4ac7de
James Hulme <hulmey101@googlemail.com>
2013-06-17 22:04:38 +0100

    csv parser is now using regular expressions to get non data parameters, need to integrate it with rest of app and continue

--- biopepa_csv_parser.py
+++ biopepa_csv_parser.py
@@ -1,3 +1,4 @@
+import re
 """
 Parse results data from BioPEPA csv files
 
@@ -15,6 +16,34 @@ Expected Structure
 
 
 class BioPepaCsvParser(object):
+
+    def __init__(self):
+        self.ymin = 1000000
+        self.xmin = 1000000
+        self.ymax = 0
+        self.xmax = 0
+
+    def open_csv(self, csv):
+        with open(csv, 'r') as f:
+            contents = f.read().strip()
+            results = {}
+            results['simulator'] = re.findall('Simulator: (.*?)\n', contents)[0]
+            results['datapoints'] = re.findall('Number of data points: (.*?)\n', contents)[0]
+            results['replications'] = re.findall('Number of independent replications: (.*?)\n', contents)[0]
+            results['stop_time'] = re.findall('Stop time: (.*?)\n', contents)[0]
+            results['start_time'] = re.findall('Start time: (.*?)\n', contents)[0]
+            results['run_time'] = re.findall('Run time = (.*?)\n', contents)[0]
+            results['data'] = ('Time, ' + re.findall('Time, (.*?)\n', contents)[0]).split(',')
+            data = [line.split(',') for line in contents.split('\n') if not "#" in line]
+            transposed_data = map(None, *data)
+
+            for i, data_item in enumerate(results['data']):
+                results[str(data_item)] = transposed_data[i]
+
+            print results.keys()
+
+
+class BioPepaCsvParserOld(object):
 
     """
     self.contents - entire file contents
