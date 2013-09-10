be199c347d4a612c4b4660892a4ac546f0e57f5a
James Hulme <hulmey101@googlemail.com>
2013-09-10 12:27:27 +0100

    some small changes to the variables in csv parser

--- biopepa_csv_parser.py
+++ biopepa_csv_parser.py
@@ -22,27 +22,34 @@ class BioPepaCsvParser(object):
         self.xmin = 1000000
         self.ymax = 0
         self.xmax = 0
+        self.results_dict = {}
 
+    """
+    Returns a dictionary - 1 key for each species, and 1 for time, returning an array of integers.
+    i.e. {"foo": [1,4,9,16,25], "bar":[1,8,27,64,125], "Time":[1,2,3,4,5]}
+    """
     def parse_csv(self, csv):
         with open(csv, 'r') as f:
             contents = f.read().strip()
-            results = {}
-            results['simulator'] = re.findall('Simulator: (.*?)\n', contents)[0]
-            results['datapoints'] = re.findall('Number of data points: (.*?)\n', contents)[0]
-            results['replications'] = re.findall('Number of independent replications: (.*?)\n', contents)[0]
-            results['stop_time'] = re.findall('Stop time: (.*?)\n', contents)[0]
-            results['start_time'] = re.findall('Start time: (.*?)\n', contents)[0]
-            results['run_time'] = re.findall('Run time = (.*?)\n', contents)[0]
-            results['data'] = ('Time, ' + re.findall('Time, (.*?)\n', contents)[0]).split(',')
+            self.results_dict['simulator'] = re.findall('Simulator: (.*?)\n', contents)[0]
+            self.results_dict['datapoints'] = re.findall('Number of data points: (.*?)\n', contents)[0]
+            self.results_dict['replications'] = re.findall('Number of independent replications: (.*?)\n', contents)[0]
+            self.results_dict['stop_time'] = re.findall('Stop time: (.*?)\n', contents)[0]
+            self.results_dict['start_time'] = re.findall('Start time: (.*?)\n', contents)[0]
+            self.results_dict['run_time'] = re.findall('Run time = (.*?)\n', contents)[0]
+            self.results_dict['data'] = ('Time, ' + re.findall('Time, (.*?)\n', contents)[0]).split(',')
             data = [line.split(',') for line in contents.split('\n') if not "#" in line]
             transposed_data = map(None, *data)
-            results['results'] = {}
-            for i, data_item in enumerate(results['data']):
-                results['results'][str(data_item)] = [float(n) for n in transposed_data[i]]
+            self.results_dict['results'] = {}
+            for i, data_item in enumerate(self.results_dict['data']):
+                self.results_dict['results'][str(data_item)] = [float(n) for n in transposed_data[i]]
 
-            self.results_dict = results['results']
+            self.results_dict = self.results_dict['results']
             self.min_max_values()
 
+    """
+    To get the labels for the axis
+    """
     def min_max_values(self):
         for result in self.results_dict:
             if not result == 'Time':
