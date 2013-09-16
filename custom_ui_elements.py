1a4b60b9a465ae0074ce098b8f4fc5ab2e92b7ba
James Hulme <hulmey101@googlemail.com>
2013-09-16 10:51:06 +0100

    fixed merge conflict

--- custom_ui_elements.py
+++ custom_ui_elements.py
@@ -7,11 +7,11 @@ class BioPepaToolbar(NavigationToolbar):
 
     ON_CUSTOM_ENLARGE = wx.NewId()
     #None is the separators
-    #toolitems = [t for t in NavigationToolbar.toolitems if t[0] in ('Home', 'Pan', 'Zoom', 'Back', 'Forward', None)]
+    toolitems = [t for t in NavigationToolbar.toolitems if t[0] in ('Home', 'Pan', 'Zoom', 'Back', 'Forward', None)]
 
     def __init__(self, graph_canvas):
         super(BioPepaToolbar, self).__init__(graph_canvas)
-        self.DeleteToolByPos(6)
+        #self.DeleteToolByPos(6)
         self.AddSimpleTool(self.ON_CUSTOM_ENLARGE, _load_bitmap('stock_left.xpm'), 'Pan to the left', 'Pan graph to the left')
         wx.EVT_TOOL(self, self.ON_CUSTOM_ENLARGE, self._on_custom_enlarge)
 
