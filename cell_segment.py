d2803f99110534b403d9562afe91ce8301199dec
James Hulme <github@jkhulme.fastmail.fm>
2014-01-14 21:14:04 +0000

    animation panel is getting better

--- cell_segment.py
+++ cell_segment.py
@@ -15,22 +15,26 @@ class CellSegment(object):
         #Get an array of tuples of times when the colour changes and what
         #colour it changes to.
         num_of_segments = len(self.world.session_dict['tree_list'])
+        print self.world.session_dict['tree_list']
         change = 0
+        print self.world.session_dict['species_dict']
         self.sub_segments = []
         for i in range(0, num_of_segments):
             centre_x = tl_x
             centre_y = tl_y
             outer_x1, outer_y1 = centre_x + radius - change, centre_y
             outer_x2, outer_y2 = centre_x, 10 + change
-            self.sub_segments.append((self.world.session_dict['lines'][file_name][species+"@"+self.world.session_dict['tree_list'][i]], self.world.session_dict['tree_list'][i], centre_x, centre_y, outer_x1, outer_y1, outer_x2, outer_y2))
+            line = self.world.session_dict['lines'][file_name].get(species+"@"+self.world.session_dict['tree_list'][i], None)
+            self.sub_segments.append((line, self.world.session_dict['tree_list'][i], centre_x, centre_y, outer_x1, outer_y1, outer_x2, outer_y2))
             change += radius / num_of_segments
 
     def paint(self, dc):
         species_locations = ['whole_cell']
         for (line, location, centre_x, centre_y, outer_x1, outer_y1, outer_x2, outer_y2) in self.sub_segments:
-            if location in species_locations or species_locations == ['whole_cell']:
-                line.update_animation_colour(self.world.session_dict['clock'])
-                dc.SetBrush(wx.Brush(line.seg_colour))
+            if line != None:
+                if location in species_locations or species_locations == ['whole_cell']:
+                    line.update_animation_colour(self.world.session_dict['clock'])
+                    dc.SetBrush(wx.Brush(line.seg_colour))
             else:
                 dc.SetBrush(wx.Brush('white'))
             dc.DrawArc(outer_x1, outer_y1, outer_x2, outer_y2, centre_x, centre_y)
