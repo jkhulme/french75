ece0d3d3f4785ba3db356964a6664e52b490a900
James Hulme <github@jkhulme.fastmail.fm>
2014-02-28 11:24:17 +0000

    calling reorder when sending stuff in the client seems to resolve conflicts, its not the fastest tool in the shed

--- rpc_client.py
+++ rpc_client.py
@@ -45,6 +45,7 @@ class French75Client():
         self.world.lamport_clock += 1
         #self.server.update_annotation(self.world.lamport_clock, pickle.dumps(a_id), pickle.dumps(text))
         self.server.update_annotation(2, pickle.dumps(a_id), pickle.dumps(text))
+        self.world.reorder(2)
 
     def launch_large_plot(self):
         """
