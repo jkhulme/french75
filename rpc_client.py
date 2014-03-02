78997d59a3123806d033a931bcc9669afdaa1b8a
James Hulme <github@jkhulme.fastmail.fm>
2014-03-02 16:23:34 +0000

    removed the calls to reordre in rpc client

--- rpc_client.py
+++ rpc_client.py
@@ -35,15 +35,12 @@ class French75Client():
         """
         works
         """
-        self.world.lamport_clock += 1
-        self.world.reorder(self.world.lamport_clock)
         self.server.add_annotation(self.world.lamport_clock, pickle.dumps(annotation))
 
     def update_annotation(self, a_id, text):
         """
         works
         """
-        self.world.lamport_clock += 1
         self.server.update_annotation(self.world.lamport_clock, pickle.dumps(a_id), pickle.dumps(text))
 
     def launch_large_plot(self):
@@ -62,63 +59,43 @@ class French75Client():
         """
         works
         """
-        self.world.lamport_clock += 1
-        self.world.reorder(self.world.lamport_clock)
         self.server.update_legend(self.world.lamport_clock, pickle.dumps((line, file_key, species_key)))
 
     def reset_session(self):
         self.server.reset_session()
 
     def undo(self):
-        self.world.reorder(self.world.lamport_clock)
         self.server.undo()
 
     def redo(self):
-        self.world.reorder(self.world.lamport_clock)
         self.server.redo()
 
     def delete_anime_annotation(self, a_id):
-        self.world.lamport_clock += 1
         self.server.delete_anime_annotation(self.world.lamport_clock, a_id)
 
     def add_anime_annotation(self, (key, annotation)):
-        self.world.lamport_clock += 1
         self.server.add_anime_annotation(self.world.lamport_clock, pickle.dumps((key, annotation)))
 
     def delete_annotation(self, a_id):
         """
         works
         """
-        self.world.lamport_clock += 1
-        self.world.reorder(self.world.lamport_clock)
         self.server.delete_annotation(self.world.lamport_clock, pickle.dumps(a_id))
 
     def toggle_param(self, param, value):
-        self.world.lamport_clock += 1
-        self.world.reorder(self.world.lamport_clock)
         self.server.toggle_param(self.world.lamport_clock, param, value)
 
     def play_animation(self):
-        self.world.lamport_clock += 1
-        self.world.reorder(self.world.lamport_clock)
         self.server.play_animation(self.world.lamport_clock)
 
     def set_clock(self):
-        self.world.lamport_clock += 1
-        self.world.reorder(self.world.lamport_clock)
         self.server.set_clock(self.world.lamport_clock, pickle.dumps(self.world.session_dict['clock']))
 
     def switch_animation(self, n):
-        self.world.lamport_clock += 1
-        self.world.reorder(self.world.lamport_clock)
         self.server.switch_animation(self.world.lamport_clock, n)
 
     def change_animation_species(self, n):
-        self.world.lamport_clock += 1
-        self.world.reorder(self.world.lamport_clock)
         self.server.change_animation_species(self.world.lamport_clock, n)
 
     def change_animation_file(self, n):
-        self.world.lamport_clock += 1
-        self.world.reorder(self.world.lamport_clock)
         self.server.change_animation_file(self.world.lamport_clock, n)
