130abd48053085934661ad898f3005ccef1ae340
James Hulme <github@jkhulme.fastmail.fm>
2014-03-29 22:56:36 +0000

    Added in mutations, seems to be working really well

--- plot_query_evaluation.py
+++ plot_query_evaluation.py
@@ -5,6 +5,7 @@ from itertools import groupby
 from collections import Counter
 from math import log, sqrt
 k = 1.6
+_CORPUS_SIZE = 100
 
 def generate_line():
     line = []
@@ -37,10 +38,17 @@ def plot_line(line):
 def build_corpus():
     corpus = {}
     lines = {}
-    while len(corpus.keys()) < 1:
+    while len(corpus.keys()) < _CORPUS_SIZE:
         line = generate_line()
         lines[len(lines.keys())] = line
         corpus[len(corpus.keys())] = dict(slice_lists(line))
+
+    max_id = len(corpus.keys())
+    mutations = mutate_line(lines[0])
+    for i, mutation in enumerate(mutations):
+        lines[max_id+i] = mutation
+        corpus[max_id+i] = dict(slice_lists(mutation))
+
     return corpus, lines
 
 def slice_lists(l):
@@ -136,44 +144,53 @@ def document_freq(word, documents):
 
 def mutate_line(line):
     plot_line(line)
-
+    mutated_lines = []
     new_line = [point*3 for point in line]
+    mutated_lines.append(new_line)
     #plot_line(new_line)
 
     new_line_2 = [point*0.5 for point in line]
+    mutated_lines.append(new_line_2)
     #plot_line(new_line_2)
 
     new_line_3 = [point*0.1 for point in line]
+    mutated_lines.append(new_line_3)
     #plot_line(new_line_3)
 
     new_line_4 = add_points(line[100:])
+    mutated_lines.append(new_line_4)
     #plot_line(new_line_4)
 
     new_line_5 = add_points(line[250:])
+    mutated_lines.append(new_line_5)
     #plot_line(new_line_5)
 
     new_line_6 = add_points(line[:-100], shift=True)
+    mutated_lines.append(new_line_6)
     #plot_line(new_line_6)
 
     new_line_7 = add_points(line[:-250], shift=True)
+    mutated_lines.append(new_line_7)
     #plot_line(new_line_7)
 
     new_line_8 = [point*2 for point in add_points(line[:-250], shift=True)]
+    mutated_lines.append(new_line_8)
     #plot_line(new_line_8)
 
     new_line_9 = [point*3 for point in add_points(line[100:])]
+    mutated_lines.append(new_line_9)
     #plot_line(new_line_9)
 
     new_line_10 = [point*0.2 for point in add_points(line[:-100], shift=True)]
-    plot_line(new_line_10)
+    mutated_lines.append(new_line_10)
+    #plot_line(new_line_10)
 
-    plt.show()
+    #plt.show()
+    return mutated_lines
 
 random.seed("french75")
 corpus, lines = build_corpus()
-mutate_line(lines[0])
-
-"""
+print len(corpus.keys())
 print "Built corpus"
 inverted_index = build_inverted_index(corpus)
 print "Built inverted index"
@@ -181,13 +198,14 @@ print "Built inverted index"
 ranking = []
 
 for (doc_id, vector) in corpus.items():
-    print doc_id
     score = tf_weighted_cosine((0, corpus[0]), (doc_id, corpus[doc_id]))
     ranking.append((0, doc_id, score))
 
 ranking.sort(key=lambda tup: tup[2])
 ranking = ranking[::-1]
+print ranking
 
+"""
 best = ranking[1:11]
 worst = ranking[-10:]
 
