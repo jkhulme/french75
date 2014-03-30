0ab8ec5b985d3dc99354b07067619bde655e3aa5
James Hulme <github@jkhulme.fastmail.fm>
2014-03-30 22:43:30 +0100

    ?

--- plot_query_evaluation.py
+++ plot_query_evaluation.py
@@ -5,7 +5,7 @@ from itertools import groupby
 from collections import Counter
 from math import log, sqrt
 k = 1.6
-_CORPUS_SIZE = 100
+_CORPUS_SIZE = 10000
 
 def generate_line():
     line = []
@@ -39,17 +39,19 @@ def build_corpus():
     corpus = {}
     lines = {}
     while len(corpus.keys()) < _CORPUS_SIZE:
+        i = len(corpus.keys())
+        print str(i) + '/' + str(_CORPUS_SIZE), str(100*i/float(_CORPUS_SIZE)) + "%"
         line = generate_line()
         lines[len(lines.keys())] = line
         corpus[len(corpus.keys())] = dict(slice_lists(line))
 
-   
+
     max_id = len(corpus.keys())
     mutations = mutate_line(lines[0])
     for i, mutation in enumerate(mutations):
         lines[max_id+i] = mutation
         corpus[max_id+i] = dict(slice_lists(mutation))
-    
+
 
     return corpus, lines
 
@@ -150,10 +152,10 @@ def fuzz_line(line):
 def fuzz_point(point):
     a = point * 0.95
     b = point * 1.05
-    return random.uniform(a, b)
+    #return random.uniform(a, b)
+    return point
 
 def mutate_line(line):
-    plot_line(line)
     mutated_lines = []
     new_line = fuzz_line([point*3 for point in line])
     mutated_lines.append(new_line)
@@ -203,11 +205,42 @@ mutant_ids = range(_CORPUS_SIZE, _CORPUS_SIZE+10)
 top_20_matches = []
 top_11_matches = []
 
-experiments = 1000
+experiments = 1
 for i in range(0, experiments):
-    print str(i) + '/' + str(experiments), str(100*i/float(experiments)) + "%"
+
     corpus, lines = build_corpus()
     inverted_index = build_inverted_index(corpus)
+
+    plot_line(lines[0])
+    plot_line(lines[256])
+    plt.show()
+    plot_line(lines[0])
+    plot_line(lines[7017])
+    plt.show()
+    plot_line(lines[0])
+    plot_line(lines[8341])
+    plt.show()
+    plot_line(lines[0])
+    plot_line(lines[1380])
+    plt.show()
+    plot_line(lines[0])
+    plot_line(lines[4997])
+    plt.show()
+    plot_line(lines[0])
+    plot_line(lines[5462])
+    plt.show()
+    plot_line(lines[0])
+    plot_line(lines[6648])
+    plt.show()
+    plot_line(lines[0])
+    plot_line(lines[2298])
+    plt.show()
+    plot_line(lines[0])
+    plot_line(lines[595])
+    plt.show()
+    plot_line(lines[0])
+    plot_line(lines[190])
+    plt.show()
 
     ranking = []
     avgd = avg_doc_length(corpus.items())
@@ -220,6 +253,7 @@ for i in range(0, experiments):
     ranking = ranking[::-1]
 
     top_20 = ranking[:21]
+    print top_20
     top_11 = ranking[:11]
     mutants = 0
     for (q_id, d_id, score) in top_20:
